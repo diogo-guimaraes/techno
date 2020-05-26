d8609fccdf6d156a0ff2a9b76b8062886360ee3f
DIogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-05-25 21:55:17 -0400

    refactor(doc): app.js
    inicia uma função construtora para fazer requisições feth em produtos,json, usando a direta hooks created para rendenrizar os dados.

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 // refactor(doc): app.js
 // add(doc): app.js
 // rm(doc): app.js
+const vm = new Vue({
+  el: "#app",
+  data: {
+    produtos: []
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then(r => r.json())
+        .then(r => {
+          this.produtos = r;
+        })
+    }
+  },
+  created() {
+    this.fetchProdutos();
+  }
+
+})
